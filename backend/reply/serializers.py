3b80344be977d29cf7e7dc9072cc82e500c28c86
Michael J <johnson_michael1329@yahoo.com>
2022-04-29 13:37:27 -0500

    Create comment and reply serializers.

---
+++ backend/reply/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Reply
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['id', 'user', 'comment', 'text', 'user_id']
+        depth = 1
