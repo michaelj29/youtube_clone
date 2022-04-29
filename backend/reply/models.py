5d89c158b149b6acbde3ce5a7e1fa195f73c23d3
Michael J <johnson_michael1329@yahoo.com>
2022-04-29 13:58:39 -0500

    Make migrations for reply and comment apps.

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -4,7 +4,7 @@ from authentication.models import User
 
 # Create your models here.
 class Reply(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_reply_set', on_delete=models.CASCADE)
+    comment = models.ForeignKey(User, related_name='comment_reply_set', on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
 
--- backend/reply/serializers.py
+++ backend/reply/serializers.py
@@ -4,5 +4,5 @@ from .models import Reply
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
-        fields = ['id', 'user', 'comment', 'text', 'user_id']
+        fields = ['id', 'user', 'comment', 'text']
         depth = 1
