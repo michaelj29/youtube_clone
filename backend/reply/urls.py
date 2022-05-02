e0da7abdf3c4190e14183a9bdb9b07cacb9472f6
mayragann <mayragann@gmail.com>
2022-05-02 17:36:35 -0400

    Compleated The Reply Function

--- backend/reply/urls.py
+++ backend/reply/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.create_reply),
-    path('<int:pk>/replies', views.reply_details),
+    path('', views.get_replies),
 
 ]
--- backend/reply/views.py
+++ backend/reply/views.py
@@ -2,29 +2,23 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from .models import Comment, Reply
-from .serializers import CommentSerializer, ReplySerializer
+from .models import Reply
+from .serializers import  ReplySerializer
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
-def reply_details(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
+def get_replies(request, pk):
     if request.method == 'GET':
-        reply = Reply.objects.filter(comment_id = comment.id)
+        reply = Reply.objects.filter(comment_id = pk)
         serializer = ReplySerializer(reply, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def post_comments(request):
-
-    if request.method == 'POST':
-        serializer = CommentSerializer(data=request.data)
+    elif request.method == 'POST':
+        serializer = ReplySerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save(user=request.user, comment_id = pk)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
