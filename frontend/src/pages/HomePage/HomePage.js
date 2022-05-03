d1f4c971dae22cc6495949e382f55491a983651d
mayragann <mayragann@gmail.com>
2022-05-03 15:45:51 -0400

    Changed NavBar

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -7,24 +7,23 @@ import axios from "axios";
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchVideos = async () => {
       try {
         let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setVideos(response.data);
       } catch (error) {
         console.log(error.message);
       }
     };
-    fetchCars();
+    fetchVideos();
   }, [token]);
   return (
     <div className="container">
