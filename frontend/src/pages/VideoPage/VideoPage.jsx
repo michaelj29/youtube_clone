fa57a5ccae2817ce1bff60586d49ee26f8c790a0
Michael J <johnson_michael1329@yahoo.com>
2022-05-05 11:20:31 -0500

    Pulling partners code.

--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,32 +1,20 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { KEY } from "../../localKey";
+import { KEY } from "../../localKey"
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 
 const VideoPage = () => {
     const params = useParams()
-    const defaultVideo = params && params.videoId
     const [videos, setVideos] = useState([]);
-    const [videoId, setVideoId] = useState('defaultVideo')
-    // now that you have the id from the url,make a get request ot get the video link
-    // const videoId = params && params.videoId
-    
 
-    // const navigate = useNavigate()
-    // navigate(`/video/${videoId}`)
+    // now that you have the id from the url,make a get request ot get the video link
+    const videoId = params && params.videoId
     
-    const handleClick = (video) => {
-        setVideoId(video)
-    }
-      
     useEffect(() => {
         const fetchVideos = async () => {
           try {
             let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&type=video&relatedToVideoId=${videoId}`, {
-              // headers: {
-              //   Authorization: "Bearer " + token,
-              // },
             });
             console.log(response.data.items)
             setVideos(response.data.items);
@@ -39,19 +27,21 @@ const VideoPage = () => {
 
     return ( 
         <div>
-            <iframe id="ytplayer" type="text/html" width="640" height="360"
-  src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
-  frameborder="0"></iframe>
+            <iframe 
+            id="ytplayer" 
+            type="text/html" 
+            width="640" 
+            height="360"
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
+            frameborder="0">
+  </iframe>
          <div>
              Related Videos Go Here
              {videos.map((video) => {
-                console.log(videoId)
-               return (
-
+                return (
                     <div>
-
                         <div>
-                        <img src={video.snippet.thumbnails.default.url} onClick={()=>handleClick(video.id.videoId)} alt="" />
+                        <img src={video.snippet.thumbnails.default.url} alt="" />
                         </div>
                         <div>
                             <h1>{video.snippet.title}</h1>
@@ -59,7 +49,7 @@ const VideoPage = () => {
                         <div>
                             <p>{video.snippet.description}</p>
                         </div>
-
+                        <Link to={`/video/${videoId}`}>Go To Video</Link>
                     </div>
                 );
             })}
