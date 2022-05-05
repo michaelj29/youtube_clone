72b7f9b4c0c9f4decaf2a0a93b4c42ff9069dccd
Michael J <johnson_michael1329@yahoo.com>
2022-05-05 14:45:46 -0500

    Initialize SearchResults Page + SearchBar Functionality.

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Form, FormControl } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import { KEY } from '../../localKey'
 
 
 const SearchBar = () => {
@@ -13,6 +16,23 @@ const SearchBar = () => {
         }
     }
 
+    useEffect(() => {
+        const fetchVideos = async () => {
+          try {
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${search}&key=${KEY}&part=snippet&type=video&maxResults=2`, {
+              // headers: {
+              //   Authorization: "Bearer " + token,
+              // },
+            });
+            console.log(response.data.items)
+            setSearch(response.data.items);
+          } catch (error) {
+            console.log(error.message);
+          }
+        };
+         fetchVideos();
+      }, []);
+
 
     return ( 
         <div>
@@ -22,8 +42,12 @@ const SearchBar = () => {
                 placeholder="Search any topic"
                 className="me-2"
                 aria-label="Search"
+                onChange={(event) => setSearch(event.target.value)} 
+                value={search}
                 />
-                <Button variant="outline-success">Search</Button>
+                <Link to="/searchResults">
+                    <Button type="submit" variant="outline-success">Search</Button>
+                </Link>
             </Form>
         </div>
 
--- frontend/src/pages/SearchResultsPage/SearchResultsPage.js
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { KEY } from '../../localKey'
 
-const SearchResultsPage = () => {
+const SearchResultsPage = ({search}) => {
 
+    const [ videos, setVideos ] = useState([])
+
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+          try {
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${search}&key=${KEY}&part=snippet&type=video&maxResults=2`, {
+              // headers: {
+              //   Authorization: "Bearer " + token,
+              // },
+            });
+            console.log(response.data.items)
+            setVideos(response.data.items);
+          } catch (error) {
+            console.log(error.message);
+          }
+        };
+         fetchVideos();
+      }, []);
 
 
     return ( 
         <div>
-            Your search Results Below
+          {console.log(videos)}
 
         </div>
      );
