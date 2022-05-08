2a3c28dfee2f1591f9a10014fc9a89df0a43d46e
Michael J <johnson_michael1329@yahoo.com>
2022-05-08 14:35:51 -0500

    Create Link around video image and title on the DisplayVideoPage.

--- frontend/src/components/DisplayVideos/DisplayVideos.jsx
+++ frontend/src/components/DisplayVideos/DisplayVideos.jsx
@@ -4,18 +4,19 @@ import { Link } from "react-router-dom";
 const DisplayVideos = ({videos}) => {
 
     return ( 
-        <div>DISPLAY
+        <div>
             {videos.map((video, index) => {
                 // get video id
                 return (
                     <div style={{'margin-bottom': '80px'}}>
-                        <div key={index}>
-                             <img src={video.snippet.thumbnails.medium.url} alt="" />
-                        </div>
-                        <div className="display-title">
-                            <h1>{video.snippet.title}</h1>
-                        </div>
-                        <Link to={`/video/${video.id.videoId}`}>Go To Video</Link>
+                        <Link to={`/video/${video.id.videoId}`}>
+                            <div key={index}>
+                                <img src={video.snippet.thumbnails.medium.url} alt="" />
+                            </div>
+                            <div className="display-title">
+                                <h1>{video.snippet.title}</h1>
+                            </div> 
+                        </Link>
                     </div>
                 );
             })}
